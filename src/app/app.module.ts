bbe1f41f5473afa5a66b7312ccf0c459b9abde2c
Unknown <iross01@qub.ac.uk>
2017-08-07 20:12:39 +0100

    initial layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainContentComponent } from './main-content/main-content.component';
+import { SetupComponent } from './main-content/setup/setup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainContentComponent,
+    SetupComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/main-content/setup/setup.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'setup',
+  templateUrl: './setup.component.html',
+  styleUrls: ['./../../app.component.scss']
+})
+export class SetupComponent {
+}
