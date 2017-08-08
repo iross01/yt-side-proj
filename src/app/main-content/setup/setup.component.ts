cb8b37b99394b4d336b0066e331253cf1c8c8059
Unknown <iross01@qub.ac.uk>
2017-08-08 14:47:34 +0100

    step 1 transition

--- src/app/main-content/setup/setup.component.ts
+++ src/app/main-content/setup/setup.component.ts
@@ -6,4 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./../../app.component.scss']
 })
 export class SetupComponent {
+
+	currentStep:number=0;
+
+	public nextStep(){
+		this.currentStep++;
+	}
 }
